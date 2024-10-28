e4ca2e912f38da02f54698d7bcb0c7b619cdc44d
Simon Nguthu <simon.nguthu@student.moringaschool.com>
2024-10-28 15:32:59 +0300

    .

--- server/app.py
+++ server/app.py
@@ -14,13 +14,84 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET'])
 def messages():
-    return ''
+    messages_array = []
+    messages = Message.query.order_by(Message.created_at.asc()).all()
+    for message in messages:
+        message_serialized = message.to_dict()
+        messages_array.append(message_serialized)   
+    
+    response = make_response(
+        jsonify(messages_array),200
+    )
+    return response
+        
+@app.route('/messages', methods=['POST'])
+def create_message():
+    new_message = Message(
+        body=request.json.get("body"),
+        username=request.json.get("username"),
+    )
 
-@app.route('/messages/<int:id>')
+    db.session.add(new_message)
+    db.session.commit()
+
+    new_message_serialized = new_message.to_dict()
+
+    response = make_response(
+        jsonify(new_message_serialized),
+        201
+    )
+
+    return response
+
+@app.route('/messages/<int:id>', methods=['GET','PATCH','DELETE'])
 def messages_by_id(id):
-    return ''
+    
+    message = Message.query.filter_by(id=id).first()
+    
+    if request.method == 'GET':
+        message_serialized = message.to_dict()
+        
+        response = make_response(
+        jsonify(message_serialized),
+        200
+         )
+
+        return response
+
+    elif request.method == 'PATCH':
+        for attr in request.json:
+            setattr(message,attr,request.json.get(attr))
+            
+        db.session.add(message)
+        db.session.commit()
+        
+        message_serialized = message.to_dict()
+        
+        response = make_response(
+        jsonify(message_serialized),
+        200
+         )
+
+        return response
+    
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit ()
+        
+        response_body = {
+            "delete_successful": True,
+            "message": "Review deleted."    
+        }
+
+        response = make_response(
+            jsonify(response_body),
+            200
+        )
+
+        return response
 
 if __name__ == '__main__':
     app.run(port=5555)
